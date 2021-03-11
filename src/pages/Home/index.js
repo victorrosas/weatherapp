902bf035ecfd3cc37d994baa1f1006c246932fde
Victor Rosas <victor.rosas.96@hotmail.com>
2021-03-11 15:17:54 -0500

    start, drawer navigation, screens, menu cmpont

---
+++ src/pages/Home/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { SafeAreaView, Text, StyleSheet } from 'react-native';
+
+import Menu from '../../components/Menu';
+
+export default function Home() {
+    return (
+        <SafeAreaView style={styles.container}>
+            <Menu />
+        </SafeAreaView>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#e8f0ff',
+        paddingTop: '5%'
+    }
+});
